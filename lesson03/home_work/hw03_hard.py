2e8840bcf96091f11f37216bc4fe2016c6238153
Dmitry <jinrlit@gmail.com>
2019-02-20 22:47:59 +0300

    Реализация функции по выполнению операций с дробями

--- lesson03/home_work/hw03_hard.py
+++ lesson03/home_work/hw03_hard.py
@@ -8,97 +8,64 @@
 # Вывод: 1 17/42  (результат обязательно упростить и выделить целую часть)
 # Ввод: -2/3 - -2
 # Вывод: 1 1/3
+
+
 def solve_fraction_equation(eq: str):
     """
     Фунцкия выполняющая операции (сложение и вычитание) с простыми дробями.
     Дроби вводятся и выводятся в формате:
     n x/y ,где n - целая часть, x - числитель, у - знаменатель.
     :param eq: строка содержащая дробное уравнение
-    :return:
+    :return: строка содержащая дробь
     """
 
     def parse_equation(str: str):
-        """Парсит строку с уравнением и возвращает по словарю от каждой дроби"""
+        """
+        Парсит строку с уравнением в список из двух дробей
+        :param str: Строка с уравнением
+        :return:
+        """
         str_like_list = str.split(' ')
-
-        fract1 = {
-            "n": None,
-            "x": None,
-            "y": None,
-        }
-        fract2 = fract1.copy()
-        is_first = True
+        first_part = True
+        fract1 = []
+        fract2 = []
         while len(str_like_list) > 0:
             current = str_like_list.pop(0)
-            if is_first:
-                if '/' in current:
-                    fract1["x"] = int(current.split('/')[0])
-                    fract1["y"] = int(current.split('/')[1])
-                elif current == '+':
-                    is_first = False
-                    is_addition = True
-                elif current == '-':
-                    is_first = False
-                    is_addition = False
+            if first_part:
+                if current == '+':
+                    operation = 'add'
+                    first_part = False
+                    continue
+                if current == '-':
+                    operation = 'sub'
+                    first_part = False
+                    continue
+                if '/' not in current:
+                    fract1.append([int(current), 1])
                 else:
-                    fract1["n"] = int(current)
+                    fract1.append(list(map(int, current.split('/'))))
             else:
-                if '/' in current:
-                    xy = list(map(int, current.split('/')))
-                    fract2["x"] = xy[0]
-                    fract2["y"] = xy[1]
+                if '/' not in current:
+                    fract2.append([int(current), 1])
                 else:
-                    fract2["n"] = int(current)
+                    fract2.append(list(map(int, current.split('/'))))
 
-        for fract in [fract1, fract2]:
-            if fract["n"] == None and fract["x"] == None:
+        # Проверка дробей на валидность и подготовка перед выводом
+        result = []
+        for fract in fract1, fract2:
+            #  Если в списке дроби не один или два элемента, то парсинг прошел некорректно и нужно выдать ошибку
+            if len(fract) not in [1, 2]:
                 return None
-            if fract["n"] == None:
-                fract["n"] = 0
-            if fract["y"] == 0:
-                return None
+            elif len(fract) == 2:
+                # Приведение целой части в числитель
+                fract_new = fract_sum(fract[0], fract[1])
+                fract = fract_new
+            else:
+                fract= fract[0]
+            result.append(fract)
 
-        return [fract1, fract2, is_addition]
-
-
-
-
-    def fract_part_unhole(fract):
-        """Приведение целой части дроби в числитель"""
-        return {
-            "n": 0,
-            "x": fract["x"] + fract["y"] * fract["n"],
-            "y": fract["y"],
-        }
-
-    def fract_part_hole(fract):
-        """Выделение целой части из дроби"""
-        n = fract["x"] // fract["y"]
-        if fract["x"] > 0:
-            x = fract["x"] % fract["y"]
-        else:
-            x = - ((-1 * fract["x"]) % fract["y"])
-        if x == 0:
-            x = y = None
-        else:
-            y = fract["y"]
-
-        return {"n": n, "x": x, "y": y}
-
-    def fract_reduct(fract):
-        """Сокращение дроби"""
-        if fract["x"]:
-            i = max(fract["x"], fract["y"])
-            while i > 2:
-                if fract["x"] // i != 0 and fract["y"] // i != 0:
-                    fract["x"] //= i
-                    fract["y"] //= i
-                    break
-                else:
-                    i -= 1
-            return fract
-        else:
-            return {"n": fract["n"], "x": None, "y": None}
+        result.append(operation)
+        return result
 
     def fract_multiply(lst1: list, lst2: list):
         """
@@ -110,29 +77,59 @@ def solve_fraction_equation(eq: str):
         result = [lst1[0]*lst2[0], lst1[1]*lst2[1]]
         return result
 
+    def fract_sum(f1, f2):
+        """
+        Сложение дробей
+        :param f1: дробь в виде списка: [числитель, знаменатель]
+        :param f2: дробь в виде списка: [числитель, знаменатель]
+        :return: дробь в виде списка: [числитель, знаменатель]
+        """
+        return [f1[0]*f2[1] + f1[1]*f2[0], f1[1]*f2[1]]
 
-    def fract_sum(fract1, fract2):
-        """Сложение дробей"""
-        return {
-            "n": 0,
-            "x": fract1["x"]*fract2["y"] + fract2["x"]*fract1["y"],
-            "y": fract1["y"]*fract2["y"]
-        }
+    def fract_sub(f1, f2):
+        """
+        Вычитание дробей
+        :param f1: дробь в виде списка: [числитель, знаменатель]
+        :param f2: дробь в виде списка: [числитель, знаменатель]
+        :return: дробь в виде списка: [числитель, знаменатель]
+        """
+        return [f1[0]*f2[1] - f1[1]*f2[0], f1[1]*f2[1]]
 
-    def fract_sub(fract1, fract2):
-        """Вычитание дробей"""
-        return {
-            "n": 0,
-            "x": fract1["x"] * fract2["y"] - fract2["x"] * fract1["y"],
-            "y": fract1["y"] * fract2["y"]
-        }
+    def fract_represent(f):
+        """
+        Отображение дроби в виде строки.
+        Перед отображением идет сокращение и выделение целой части из дроби
+        :param f: дробь в виде списка: [числитель, знаменатель]
+        :return:
+        """
 
-    def fract_represent(fract):
-        """Отображение дроби в виде строки"""
-        if fract["x"]:
-            result = "{} {}/{}".format(fract["n"], fract["x"], fract["y"])
+        # Если числитель отрицательный - приводим к положительному.
+        # Это позволит пользоваться делением от остатка.
+        if f[0] < 0:
+            sign = -1
+            f[0] *= -1
         else:
-            result = fract["n"]
+            sign = 1
+
+        # Сокращение дроби.
+        # Ищем максимальный общий делитель. Перебираем по низпадающей до 1
+        i = min(f)
+        while i > 1:
+            if f[0] % i == 0 and f[1] % i == 0:
+                f[0] //= i
+                f[1] //= i
+                break
+            i -= 1
+
+        # Выделение целой части
+        hole = f[0] // f[1]
+        f[0] %= f[1]
+
+        # Вывод с целой частью или без. Приводим к отрицательному если был
+        if hole != 0:
+            result = "{} {}/{}".format(hole * sign, f[0], f[1])
+        else:
+            result = "{}/{}".format(f[0] * sign, f[1])
 
         return result
 
@@ -142,23 +139,20 @@ def solve_fraction_equation(eq: str):
 
     fract1 = fractions[0]
     fract2 = fractions[1]
-    is_addition = fractions[2]
-
-    # Избавляемся от целых частей дроби
-    fract1 = fract_part_unhole(fract1)
-    fract2 = fract_part_unhole(fract2)
+    operation = fractions[2]
 
     # Производим сложение или вычитание в зависимости от требуемой операции
-    if is_addition:
+    if operation == 'add':
         fract3 = fract_sum(fract1, fract2)
     else:
         fract3 = fract_sub(fract1, fract2)
 
-    return fract_represent(fract_reduct(fract_part_hole(fract3)))
+    # Выводим результат
+    return fract_represent(fract3)
 
 
-# print(solve_fraction_equation("2 1/3 + 2 2/3"))
-print(solve_fraction_equation("2 1/3 - 2/3"))
+print(solve_fraction_equation("5/6 + 4/7"))
+print(solve_fraction_equation("-2/3 - -2"))
 
 # Задание-2:
 # Дана ведомость расчета заработной платы (файл "data/workers").
