5049daf19e9f221919beea5b9a769ed9e75d539d
Dmitry <jinrlit@gmail.com>
2019-03-04 17:24:03 +0300

    реализована и оформлена задача  hard-1

--- lesson06/home_work/hw06_hard.py
+++ lesson06/home_work/hw06_hard.py
@@ -16,19 +16,22 @@
 """
 
 
-class Workers:
-    """Класс рабочий"""
-    # Список созданных объектов класса Worker
-    workers = []
+class Employee:
+    """Класс сотрудник"""
+    # Список созданных объектов класса Employee
+    employees = []
 
     def __init__(self, raw_line: str):
-        worker_data = raw_line.split()
-        self.first_name = worker_data[0]
-        self.last_name = worker_data[1]
-        self.salary = int(worker_data[2])
-        self.position = worker_data[3]
-        self.hours_expected = int(worker_data[4])
-        self.workers.append(self)
+        employee_data = raw_line.split()
+        self.first_name = employee_data[0]
+        self.last_name = employee_data[1]
+        self.salary = int(employee_data[2])
+        self.position = employee_data[3]
+        self.hours_expected = int(employee_data[4])
+        self.hours_worked_out = None
+        self.income = None
+        self.employees.append(self)
+
         pass
 
     @staticmethod
@@ -36,29 +39,67 @@ class Workers:
         """
         Добавить количество отработанных часов.
         Строка на вход парсится.
-        Если она является строкой с И.О. рабочего и его отработанными часами - ищется подходящий объект
+        Если она является строкой с И.О. сотрудника и его отработанными часами - ищется подходящий объект
         с тим же И.О. и добавляется информация об отработанных часах.
-        :param raw_line: строка, содержащая И.О. рабочего и количество отработанных часов
+        :param raw_line: строка, содержащая И.О. сотрудника и количество отработанных часов
         :return: None
         """
+        # Парсим строку
         worker_hours = raw_line.split()
-        # TODO реализовать метод
-        pass
+        first_name = worker_hours[0]
+        last_name = worker_hours[1]
+        hours_worked_out = int(worker_hours[2])
+
+        for worker in Employee.employees:
+            if first_name == worker.first_name and last_name == worker.last_name:
+                worker.hours_worked_out = hours_worked_out
+                worker.calculate_salary()
+                return
+
+    @staticmethod
+    def employees_info():
+        """Отображает информацию о всех сотрудникаф: Ф.И, должность, оклад, отработано, ЗП"""
+        print("{:18} {:^12} {:^10} {:10} {:^6}".format("Ф.И.", "Должность", "Оклад", "Отработано", "ЗП"))
+        for emp in Employee.employees:
+            print("{:18} {:12} {:^10} {:^10} {:0.2f}".format(
+                f"{emp.last_name} {emp.first_name}",
+                emp.position,
+                emp.salary,
+                emp.hours_worked_out,
+                emp.income
+            ))
 
     def calculate_salary(self):  # TODO реализовать метод
-        """Высчитать зарплату за месяц"""
-        pass
+        """Высчитать зарплату сотрудника за месяц"""
+        # Почасовой оклад
+        per_hour_salary = self.salary / self.hours_expected
+        if self.hours_expected >= self.hours_worked_out:
+            # Сумма за отработанные часы до нормы
+            self.income = self.hours_worked_out * per_hour_salary
+        else:
+            # Сумма за отработанные часы сверх нормы
+            # xp + 2p(y-x) = p(2y - x), где x - норма, y - выработка, p - per_hour salary
+            self.income = per_hour_salary * (2 * self.hours_worked_out - self.hours_expected)
+
+    def get_info(self):
+        """Отображает информацию о сотруднике"""
 
 
 if __name__ == '__main__':
+
+    # Считываем базовую информацию о рабочих
     with open("data/workers", 'r', encoding='utf-8') as f:
         f.readline()
         for line in f.readlines():
-            Workers(line.strip())
+            Employee(line.strip())
 
+    # Считываем информацию об отработанных часах
     with open("data/hours_of", 'r', encoding='utf-8') as f:
         f.readline()
         for line in f.readlines():
-            Workers.add_hours(line.strip())
+            Employee.add_hours(line.strip())
+
+    # Отображаем расчетную зарплату по всем сотрудникам
+    Employee.employees_info()
 
 
